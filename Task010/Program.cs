8ad5968347a2ba31115abe81dee547b85bbf4c24
anna <nuta5691@mail.ru>
2023-08-09 20:29:58 +0300

    add Task010

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -3,3 +3,21 @@
 // 782 -> 8
 // 918 -> 1
 
+int SecondDigit (int num)
+{
+    int doubleDigit = num/10;
+    int secondDigit = doubleDigit%10;
+    return secondDigit;
+}
+
+Console.WriteLine ("Введите трехзначное число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+if (number >= 100 && number <= 999)
+{
+    int result = SecondDigit(number);
+    Console.WriteLine ($"Вторая цифра введенного числа: {result}");
+}
+else
+{
+    Console.WriteLine ($"Ошибка ввода");
+}
